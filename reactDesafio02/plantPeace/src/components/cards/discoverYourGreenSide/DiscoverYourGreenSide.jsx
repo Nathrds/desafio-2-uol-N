4e903b2c98f8dd59f34593d38d381d01e737b2a6
MatheusHAlvess <matheus.henriquealves@ufape.edu.br>
2023-12-29 12:37:44 -0300

    Added button and adjusted positioning of some elements in Discover on the homepage

--- reactDesafio02/plantPeace/src/components/cards/discoverYourGreenSide/DiscoverYourGreenSide.jsx
+++ reactDesafio02/plantPeace/src/components/cards/discoverYourGreenSide/DiscoverYourGreenSide.jsx
@@ -1,9 +1,11 @@
-// import React from 'react'
 import styles from './Discover.module.css'
+
+// import { NavLink } from "react-router-dom"
+
 import RightImage from "../../../assets/img/imgDiscovery/right plant p.png";
 import LeftImage from "../../../assets/img/imgDiscovery/left plant p.png";
-// import Line from "../../../assets/img/imgDiscovery/line.svg";
 
+import SquareButton from "../../buttons/squareButton/SquareButton"
 
 const DiscoverYourGreenSide = () => {
   return (
@@ -12,15 +14,23 @@ const DiscoverYourGreenSide = () => {
             <h1 className={styles.h1}><div className={styles.Line}></div>Love for Nature</h1>
             <h2 className={styles.h2}>Discover Your <span className={styles.green}>Green</span> Side</h2>
             <p className={styles.p}>We are you one-stop destination for all things green and growing. Our website
-                offers a array of stunning plants, ranging from vibrant flowers to lush indoor foliage,
+                offers a array of stunning plants, ranging from vibrant flowers to lush indoor<br/>foliage,
                 allowing you to create your own green oasis.
             </p>
-            <div className={styles.img1}>
+            <section className={styles.button}>
+                <a href="#sectionScroll">
+                    <SquareButton title={"Shop now"} />
+                </a>
+            </section>
+            <section className={styles.learn}>
+                <p className={styles.p2}>Learn Gardening &rarr;</p>
+            </section>
+            <div className={styles.img2}>
                 <img className={styles.image2} src={LeftImage} alt="Left plant" />
             </div>
         </div>
         
-        <div className={styles.img2}>
+        <div className={styles.img1}>
             <img className={styles.image1} src={RightImage} alt="Right plant" />
         </div>
  </div>
