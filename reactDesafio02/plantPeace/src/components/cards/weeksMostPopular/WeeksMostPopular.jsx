ab4bbba5a53bbbb9977f8f9bc2747558742a9270
Thiago Almeida <thiagoalmeidadepaiva@gmail.com>
2023-12-28 22:55:52 -0300

    adding style to the title

--- reactDesafio02/plantPeace/src/components/cards/weeksMostPopular/WeeksMostPopular.jsx
+++ reactDesafio02/plantPeace/src/components/cards/weeksMostPopular/WeeksMostPopular.jsx
@@ -6,13 +6,16 @@ import style from "./WeeksMostPopular.module.css"
 const WeeksMostPopular = () => {
     return (
         <div className={style.container}>
-            <section>
-                <h1 className={style.h1}>this weeks Most Popular and best selling</h1>
+            <section className={style.sectionWeeks}>
+                <h1 className={style.sectionWeeks}>
+                    <span className={style.lunarGreenText}>This Weeks Most Popular</span>
+                    <span className={style.avacadoText}> And Best Selling</span>
+                </h1>
             </section>
             <section className={style.cards}>
-            <CardWeeksMostPopular image={dataPlants["plants"][0]["imgUrl"]} title={dataPlants["plants"][0]["name"]} price={dataPlants["plants"][0]["price"]}/>
-            <CardWeeksMostPopular image={dataPlants["plants"][0]["imgUrl"]} title={dataPlants["plants"][0]["name"]} price={dataPlants["plants"][0]["price"]}/>
-            <CardWeeksMostPopular image={dataPlants["plants"][0]["imgUrl"]} title={dataPlants["plants"][0]["name"]} price={dataPlants["plants"][0]["price"]}/>
+                <CardWeeksMostPopular image={dataPlants["plants"][0]["imgUrl"]} title={dataPlants["plants"][0]["name"]} price={dataPlants["plants"][0]["price"]}/>
+                <CardWeeksMostPopular image={dataPlants["plants"][0]["imgUrl"]} title={dataPlants["plants"][0]["name"]} price={dataPlants["plants"][0]["price"]}/>
+                <CardWeeksMostPopular image={dataPlants["plants"][0]["imgUrl"]} title={dataPlants["plants"][0]["name"]} price={dataPlants["plants"][0]["price"]}/>
             </section>
         </div>
         
