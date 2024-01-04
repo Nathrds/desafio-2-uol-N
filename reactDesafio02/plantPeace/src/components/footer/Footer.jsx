fc575629a0a3a9713ec73b1ff115cbe53268eb1f
Nathália <nathaliarsantos@live.com>
2024-01-04 20:16:59 -0300

    fixed responsiveness bugs in the footer, and corrected issues in the new 'About Us' page

--- reactDesafio02/plantPeace/src/components/footer/Footer.jsx
+++ reactDesafio02/plantPeace/src/components/footer/Footer.jsx
@@ -8,23 +8,31 @@ const Footer = () => {
     <div className={styles.container}>
         <div className={styles.containerInformation}>
             <article className={styles.articleFooter}>
-                <h1>Stay Fresh</h1>
-                <p>compassinhos@gmail.com</p>
-                <p>+55 41 99999-9999</p>
+                <h1 className={styles.articleFooter_H1}>Stay Fresh</h1>
+                <p className={styles.articleFooter_P}>compassinhos@gmail.com</p>
+                <p className={styles.articleFooter_P}>+55 41 99999-9999</p>
             </article>
             <nav className={styles.navFooter}>
                 <section className={styles.sectionFooter}>
-                    <h2>Links</h2>
-                    <NavLink to="/aboutus" className={styles.navLink}>About Us</NavLink>
-                    <NavLink to="/products" className={styles.navLink}>Products</NavLink>
+                    <h2 className={styles.sectionFooter_H2}>Links</h2>
+                    <NavLink to="/aboutus" className={styles.navLink}>
+                        About Us
+                    </NavLink>
+                    <NavLink to="/products" className={styles.navLink}>
+                        Products
+                    </NavLink>
                     <NavLink className={styles.navLink} to="https://www.rhs.org.uk/plants/types" target="_blank" rel="noopener noreferrer">
                         Blogs
                     </NavLink>
                 </section>
                 <section className={styles.sectionFooter}>
-                    <h2>Community</h2>
-                    <NavLink to="/aboutus" className={styles.navLink}>About Us</NavLink>
-                    <NavLink to="/products" className={styles.navLink}>Products</NavLink>
+                    <h2 className={styles.sectionFooter_H2}>Community</h2>
+                    <NavLink to="/aboutus" className={styles.navLink}>
+                        About Us
+                    </NavLink>
+                    <NavLink to="/products" className={styles.navLink}>
+                        Products
+                    </NavLink>
                     <NavLink className={styles.navLink} to="https://www.rhs.org.uk/plants/types" target="_blank" rel="noopener noreferrer">
                         Blogs
                     </NavLink>
@@ -32,8 +40,8 @@ const Footer = () => {
             </nav>
         </div>
         <footer className={styles.footerWithImg}>
-                <img src={Logo} alt="Site logo with a plant" />
-                <p>Compassinhos ®. All rights reserved.</p>
+                <img className={styles.footerWithImg_Img} src={Logo} alt="Site logo with a plant" />
+                <p className={styles.footerWithImg_P}>Compassinhos ®. All rights reserved.</p>
         </footer>
     </div>
   )
