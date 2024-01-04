352aa494f8ae7e1ffa9cc3ef895be953192dffbe
MatheusHAlvess <matheus.henriquealves@ufape.edu.br>
2024-01-04 16:56:03 -0300

    Adjusting position elements in more about plant

--- reactDesafio02/plantPeace/src/components/cards/cardMoreAboutPlant/CardMoreAboutPlant.jsx
+++ reactDesafio02/plantPeace/src/components/cards/cardMoreAboutPlant/CardMoreAboutPlant.jsx
@@ -17,7 +17,7 @@ const CardMoreAboutPlant = () => {
       </div>
       <div className={styles.containerContent2}>
         <h1 className={styles.title}>Echinocereus Cactus</h1>
-        <h2>A Majestic Addition to You Plant Collection</h2>
+        <h2 className={styles.sub}>A Majestic Addition to You Plant Collection</h2>
         <section className={styles.label}>
           <CardButton label={<span>indoor</span>} />
           <CardButton  label={<span>cactus</span>} />
@@ -30,18 +30,18 @@ const CardMoreAboutPlant = () => {
         </section>
         <section className={styles.features}>
           <div className={styles.featuresTitle}>
-            <h1>Features</h1>
+            <h1 className={styles.Title}>Features</h1>
             <ul className={styles.topics}>
-                <li>Species Echinocereus spp.</li>
-                <li>Mature Size: Varies by species, typically ranging from 4 to 12 inches (10-30 cm) in height.</li>
-                <li>Blooming Season: Typically spring or summer, with flowers lasting several days to weeks.</li>
-                <li>Pot Size: Avaliable in various pot sizes to suit your preference and needs</li>
+                <li><span className={styles.bull}>&bull;</span>Species Echinocereus spp.</li>
+                <li><span className={styles.bull}>&bull;</span>Mature Size: Varies by species, typically ranging from 4 to 12 inches (10-30 cm) in height.</li>
+                <li><span className={styles.bull}>&bull;</span>Blooming Season: Typically spring or summer, with flowers lasting several days to weeks.</li>
+                <li><span className={styles.bull}>&bull;</span>Pot Size: Avaliable in various pot sizes to suit your preference and needs</li>
             </ul>
           </div>
         </section>
         <section className={styles.description}>
           <div className={styles.descriptionTitle}>
-            <h1>Description</h1>
+            <h1 className={styles.Title}>Description</h1>
             <p className={styles.descriptionContent}>{textoComAspas}</p>
           </div>
         </section>
