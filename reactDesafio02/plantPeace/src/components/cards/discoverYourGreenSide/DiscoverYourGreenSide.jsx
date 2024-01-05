a8f3fa42314b562ee0894768ab56655ba522c9c4
Thiago Almeida <thiagoalmeidadepaiva@gmail.com>
2024-01-04 23:11:45 -0300

    finishing the responsiveness of the DiscoverYourGreenSide tab

--- reactDesafio02/plantPeace/src/components/cards/discoverYourGreenSide/DiscoverYourGreenSide.jsx
+++ reactDesafio02/plantPeace/src/components/cards/discoverYourGreenSide/DiscoverYourGreenSide.jsx
@@ -17,22 +17,24 @@ const DiscoverYourGreenSide = () => {
   return (
     <div className={styles.container}>
         <div className={styles.containerContent}>
-            <h1 className={styles.h1}><div className={styles.Line}></div>Love for Nature</h1>
-            <h2 className={styles.h2}>Discover Your <span className={styles.green}>Green</span> Side</h2>
-            <p className={styles.p}>We are you one-stop destination for all things green and growing. Our website
-                offers a array of stunning plants, ranging from vibrant flowers to lush indoor<br/>foliage,
-                allowing you to create your own green oasis.
-            </p>
-            <section className={styles.button}>
-                <a href="#sectionScroll">
-                    <SquareButton title={"Shop now"} />
-                </a>    
-            </section>
-            <section className={styles.learn}>
-                <p className={styles.p2} onClick={scrollToLearn}>Learn Gardening &rarr;</p>
-            </section>
-            <div className={styles.img2}>
-                <img className={styles.image2} src={LeftImage} alt="Left plant" />
+            <div>
+                <h1 className={styles.h1}><div className={styles.Line}></div>Love for Nature</h1>
+                <h2 className={styles.h2}>Discover Your <span className={styles.green}>Green</span> Side</h2>
+                <p className={styles.p}>We are you one-stop destination for all things green and growing. Our website
+                    offers a array of stunning plants, ranging from vibrant flowers to lush indoor<br/>foliage,
+                    allowing you to create your own green oasis.
+                </p>
+                <section className={styles.button}>
+                    <a href="#sectionScroll">
+                        <SquareButton title={"Shop now"} />
+                    </a>    
+                </section>
+            </div>               
+            <div className={styles.image2}>
+                <img src={LeftImage} alt="Left plant" />
+                <section className={styles.learn}>
+                    <p className={styles.p2} onClick={scrollToLearn}>Learn Gardening &rarr;</p>
+                </section>
             </div>
         </div>
         
